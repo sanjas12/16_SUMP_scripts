dd82480a169d435bdb26095589c67e6c0248f86d
sanja_s <sanja100v@gmail.com>
2025-03-30 17:16:45 +0300

    `Refactor TechProgram.py: add named tuple, encoding, and default time`

--- OUR/01_TechProgram/TechProgram.py
+++ OUR/01_TechProgram/TechProgram.py
@@ -1,18 +1,32 @@
+from collections import namedtuple
+
 in_file = "in_TechProgram"
 out_file = "out_TechProgram.xml"
+encoding='utf-8'
+default_time = '00/00/0000 00:00:00:000'
+name_program = 'Инвентаризация в БВ2'
 
+Cell = namedtuple('Cell', ['bridge', 'trolley'])
 list_cells = []
 
-with open(in_file, 'r', encoding='utf-8') as f_in:
+with open(in_file, 'r', encoding=encoding) as f_in:
     for line in f_in.readlines():
         if line.startswith('#'):
             continue
-        list_cells.append(tuple(line.strip().split(';')))
-
-with open(out_file, 'w', encoding='utf-8') as f_out:
-    for line in list_cells:
-        f_out.write('\t\t\t<TechObject name="CycleInventOneRI_withoutUP" operatorsname="Администратор" repeat="true" '
-                    'status="0" timestart="04/07/2022 17:29:41:761" timestop="04/07/2022 17:29:45:209">' + "\n")
-        f_out.write(f'\t\t\t\t<Param xmlns:dt="urn:schemas-microsoft-com:datatypes" dt:dt="string" CellCoords="true" '
-                    f'id="0">{line[0]};{line[1]}</Param>' + "\n")
+        cell = Cell(*tuple(line.strip().split(';')))
+        list_cells.append(cell)
+print(list_cells)
+with open(out_file, 'w', encoding=encoding) as f_out:
+    f_out.write(f'\t<TechProgram name="1. {name_program}" type="1">' + "\n")
+    f_out.write(f'\t\t<DisplayedName xmlns:dt="urn:schemas-microsoft-com:datatypes"' + 
+                f'dt:dt="string">{name_program}</DisplayedName>' + "\n")
+    f_out.write(f'\t\t<Description xmlns:dt="urn:schemas-microsoft-com:datatypes"' +
+                f'dt:dt="string">{name_program}</Description>' + "\n")
+    for cell in list_cells:
+        f_out.write(f'\t\t\t<TechObject name="CycleInventOneRI_withoutUP" operatorsname="Администратор" repeat="true" ' + 
+                    f'status="0" timestart="{default_time}" timestop="{default_time}">' + "\n")
+        f_out.write(f'\t\t\t\t<Param xmlns:dt="urn:schemas-microsoft-com:datatypes" dt:dt="string" CellCoords="true" ' + 
+                    f'id="0">{cell.bridge};{cell.trolley}</Param>' + "\n")
         f_out.write(f'\t\t\t</TechObject>' + "\n")
+
+    f_out.write(f'\t<TechProgram>' + "\n")
